3a147dd9d58a585d28e8b369ffa76a55c5710fc3
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-09-01 10:56:05 +0545

    Update FeedReader.js

--- FeedReader.js
+++ FeedReader.js
@@ -1,65 +1,66 @@
 let API = "https://api.rss2json.com/v1/api.json?rss_url=";
 
-// Define feed URLs for each category
-let feedsByCategory = {
-    'Construction News': [
-        "https://www.google.com/alerts/feeds/06313983183609550648/86313983183609550648/863422411556577025",
-        "https://www.google.com/alerts/feeds/06313983183609550648/16219506770346016876",
-        "https://www.google.com/alerts/feeds/06313983183609550648/16120492411931428893",
-        "https://www.google.com/alerts/feeds/06313983183609550648/9874521726757945814",
-        "https://www.google.com/alerts/feeds/06313983183609550648/13722836873792678019",
-        "https://www.google.com/alerts/feeds/06313983183609550648/17566939723916288150",
-        "https://www.google.com/alerts/feeds/06313983183609550648/7361213428763324582",
-        "https://www.google.com/alerts/feeds/06313983183609550648/8196542214144987233"
+// Define a mapping of RSS feed URLs for each category
+const feedURLs = {
+    'construction-news': [
+        'https://www.google.com/alerts/feeds/06313983183609550648/863422411556577025',
+        'https://www.google.com/alerts/feeds/06313983183609550648/16219506770346016876',
+        'https://www.google.com/alerts/feeds/06313983183609550648/16120492411931428893',
+        'https://www.google.com/alerts/feeds/06313983183609550648/9874521726757945814',
+        'https://www.google.com/alerts/feeds/06313983183609550648/13722836873792678019',
+        'https://www.google.com/alerts/feeds/06313983183609550648/17566939723916288150',
+        'https://www.google.com/alerts/feeds/06313983183609550648/7361213428763324582',
+        'https://www.google.com/alerts/feeds/06313983183609550648/8196542214144987233'
     ],
-    'Share Market': [
-        "https://arthasarokar.com/feed",
-        "https://arthadabali.com/feed",
-        "https://www.arthapath.com/feed",
-        "https://bajarkochirfar.com/feed",
-        "https://bizmandu.com/feed",
-        "https://www.corporatenepal.com/rss",
-        "https://www.nepsebajar.com/feed"
+    'share-market': [
+        'https://arthasarokar.com/feed',
+        'https://arthadabali.com/feed',
+        'https://www.arthapath.com/feed',
+        'https://bajarkochirfar.com/feed',
+        'https://bizmandu.com/feed',
+        'https://www.corporatenepal.com/rss',
+        'https://www.nepsebajar.com/feed'
     ],
-    'Nepali News': [
-        "https://annapurnapost.com/rss",
-        "https://www.bbc.com/nepali/index.xml",
-        "https://www.nepalpress.com/feed/",
-        "https://onlinekhabar.com/feed",
-        "https://www.ratopati.com/feed",
-        "https://www.sanghunews.com/feed",
-        "https://techpana.com/rss",
-        "https://www.ukeraa.com/feed",
-        "https://thahakhabar.com/rss",
-        "https://deshsanchar.com/feed"
+    'nepali-news': [
+        'https://annapurnapost.com/rss',
+        'https://www.bbc.com/nepali/index.xml',
+        'https://www.nepalpress.com/feed/',
+        'https://onlinekhabar.com/feed',
+        'https://www.ratopati.com/feed',
+        'https://www.sanghunews.com/feed',
+        'https://techpana.com/rss',
+        'https://www.ukeraa.com/feed',
+        'https://thahakhabar.com/rss',
+        'https://deshsanchar.com/feed'
     ]
 };
 
-// Function to fetch and display feeds
-function loadFeeds(category) {
-    let userFeedURLs = feedsByCategory[category] || [];
-    userFeedURLs.forEach(userUrl => {
-        $.ajax({
-            type: 'GET',
-            url: API + userUrl,
-            dataType: 'jsonp',
-            success: function (data) {
-                console.log(data);
-                data.items.forEach(item => {
-                    var content = document.getElementById('content');
+// Determine the category from the URL hash
+let category = window.location.hash.substring(1) || 'construction-news'; // Default to 'construction-news'
 
-                    var newItem = "";
-                    newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h1>" + item.title + "</h1></a>" + "<h4> from " + data.feed.title + "</h4>";
-                    if (item.author != "")
-                        newItem += "<h4> By " + item.author + "</h4>";
+let userFeedURLs = feedURLs[category] || [];
+userFeedURLs.forEach(userUrl => {
+    $.ajax({
+        type: 'GET',
+        url: API + userUrl,
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log(data);
+            data.items.forEach(item => {
+                var content = document.getElementById('content');
 
-                    newItem += "<h4>Published Date: " + item.pubDate + "</h4>";
+                // Create a new item container
+                var newItem = "";
+                newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h1>" + item.title + "</h1></a>";
+                
+                // Remove 'from Google Alert -...' if it exists
+                let description = item.description.replace(/from Google Alert -.*?<br>/i, '');
+                
+                newItem += "<h4>Published Date: " + item.pubDate + "</h4>";
+                newItem += description + "<hr></div>";
 
-                    newItem += item.description + "<hr></div>";
-
-                    content.insertAdjacentHTML('beforeend', newItem);
-                });
-            }
-        });
+                content.insertAdjacentHTML('beforeend', newItem);
+            });
+        }
     });
-}
+});
